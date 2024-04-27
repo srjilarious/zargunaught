e8eed78d5fd8e24794e63009c1d390439a3014a6
Jeff DeWall <sr.jilarious@fastmail.net>
2024-04-26 18:34:42 -0600

    Got a couple of config tests working after fixing testz package for zig 0.12.0 release updates.

---
+++ src/tests/option_tests.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const zargs = @import("zargunaught");
+// const fix = @import("fixtures.zig");
+const testz = @import("testz");
+
+pub fn anOptionMustHaveALongNameTest() !void {
+    _ = zargs.ArgParser.init(std.heap.page_allocator, .{ .name = "Bad option configuration", .opts = &.{
+        .{ .longName = "beta", .shortName = "b", .description = "", .maxNumParams = 1 },
+        .{ .longName = "", .shortName = "d", .description = "", .maxNumParams = 1 },
+    } }) catch |err| {
+        try testz.expectEqual(err, zargs.ParserConfigError.LongOptionNameMissing);
+        return;
+    };
+    try testz.expectTrue(false);
+}
+
+pub fn aLongOptionMustNotBeginWithANumberTest() !void {
+    _ = zargs.ArgParser.init(std.heap.page_allocator, .{ .name = "Bad option configuration", .opts = &.{
+        .{ .longName = "beta", .shortName = "b", .description = "", .maxNumParams = 1 },
+        .{ .longName = "23delta", .shortName = "d", .description = "", .maxNumParams = 1 },
+    } }) catch |err| {
+        try testz.expectEqual(err, zargs.ParserConfigError.OptionBeginsWithNumber);
+        return;
+    };
+    try testz.expectTrue(false);
+}
+
+pub fn aShortOptionMustNotBeginWithANumberTest() !void {
+    _ = zargs.ArgParser.init(std.heap.page_allocator, .{ .name = "Bad option configuration", .opts = &.{
+        .{ .longName = "beta", .shortName = "b", .description = "", .maxNumParams = 1 },
+        .{ .longName = "delta", .shortName = "23d", .description = "", .maxNumParams = 1 },
+    } }) catch |err| {
+        try testz.expectEqual(err, zargs.ParserConfigError.OptionBeginsWithNumber);
+        return;
+    };
+
+    try testz.expectTrue(false);
+}
+
