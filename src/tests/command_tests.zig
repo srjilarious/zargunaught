ed982338dcc286d3731ed834a43265af7ccd7069
Jeff DeWall <sr.jilarious@fastmail.net>
2024-05-13 06:58:12 -0600

    Split the command tests, adding a fixture function.

--- src/tests/command_tests.zig
+++ src/tests/command_tests.zig
@@ -5,8 +5,9 @@ const zargs = @import("zargunaught");
 const testz = @import("testz");
 
 // ----------------------------------------------------------------------------
-pub fn anOptionMustHaveALongNameTest() !void {
-    var parser = try zargs.ArgParser.init(std.heap.page_allocator, .{ .name = "Simple command configuration", 
+fn simpleCommandConfig() !zargs.ArgParser 
+{
+    return try zargs.ArgParser.init(std.heap.page_allocator, .{ .name = "Simple command configuration", 
         .opts = &.{
             .{ .longName = "beta", .shortName = "b", .description = "", .maxNumParams = 1 },
             .{ .longName = "delta", .shortName = "d", .description = "", .maxNumParams = 1 },
@@ -20,36 +21,42 @@ pub fn anOptionMustHaveALongNameTest() !void {
             }
         }
     }) ;
+}
+
+// ----------------------------------------------------------------------------
+// Check a command can run with global option.
+pub fn commandWithGlobalOptTest() !void {
+    var parser = try simpleCommandConfig();
     defer parser.deinit();
 
-    // Check a command can run with global option.
-    {
-        const sysv = try zargs.utils.tokenizeShellString(std.heap.page_allocator, "--beta 123 fire");
-        defer std.heap.page_allocator.free(sysv);
+    const sysv = try zargs.utils.tokenizeShellString(std.heap.page_allocator, "--beta 123 fire");
+    defer std.heap.page_allocator.free(sysv);
 
-        const args = try parser.parseArray(sysv);
-        try testz.expectEqual(args.options.items.len, 1);
-        try testz.expectEqualStr(args.options.items[0].name, "beta");
+    const args = try parser.parseArray(sysv);
+    try testz.expectEqual(args.options.items.len, 1);
+    try testz.expectEqualStr(args.options.items[0].name, "beta");
 
-        try testz.expectTrue(args.command != null);
-        try testz.expectEqualStr(args.command.?.name, "fire");
-    }
+    try testz.expectTrue(args.command != null);
+    try testz.expectEqualStr(args.command.?.name, "fire");
+    
+}
 
-    // Check a command can run with a local option.
-    {
-        const sysv = try zargs.utils.tokenizeShellString(std.heap.page_allocator, "--beta 123 transmogrify -i stone");
-        defer std.heap.page_allocator.free(sysv);
+// ----------------------------------------------------------------------------
+// Check a command can run with a local option.
+pub fn commandWithCommandOptTest() !void {
+    var parser = try simpleCommandConfig();
+    defer parser.deinit();
+    const sysv = try zargs.utils.tokenizeShellString(std.heap.page_allocator, "--beta 123 transmogrify -i stone");
+    defer std.heap.page_allocator.free(sysv);
 
-        const args = try parser.parseArray(sysv);
-        
-        try testz.expectTrue(args.command != null);
-        try testz.expectEqualStr(args.command.?.name, "transmogrify");
+    const args = try parser.parseArray(sysv);
+    
+    try testz.expectTrue(args.command != null);
+    try testz.expectEqualStr(args.command.?.name, "transmogrify");
 
-        try testz.expectEqual(args.options.items.len, 2);
-        try testz.expectEqualStr(args.options.items[0].name, "beta");
-        try testz.expectEqualStr(args.options.items[0].values.items[0], "123");
-        try testz.expectEqualStr(args.options.items[1].name, "into");
-        try testz.expectEqualStr(args.options.items[1].values.items[0], "stone");
-
-    }
+    try testz.expectEqual(args.options.items.len, 2);
+    try testz.expectEqualStr(args.options.items[0].name, "beta");
+    try testz.expectEqualStr(args.options.items[0].values.items[0], "123");
+    try testz.expectEqualStr(args.options.items[1].name, "into");
+    try testz.expectEqualStr(args.options.items[1].values.items[0], "stone");
 }
