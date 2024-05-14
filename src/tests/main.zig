156da80815b36e8c3dee3cc4001c0a942f46cbad
Jeff DeWall <sr.jilarious@fastmail.net>
2024-05-14 06:51:44 -0600

    Added license and fixed tests to follow latest testz API.

--- src/tests/main.zig
+++ src/tests/main.zig
@@ -3,13 +3,13 @@ const zargs = @import("zargunaught");
 const testz = @import("testz");
 
 const Tests = testz.discoverTests(.{
-    @import("./utils_tests.zig"),
-    @import("./option_tests.zig"),
-    @import("./command_tests.zig"),
+    testz.Group{ .name = "Utility tests", .tag = "utils", .mod = @import("./utils_tests.zig") },
+    testz.Group{ .name = "Options tests", .tag = "option", .mod = @import("./option_tests.zig") },
+    testz.Group{ .name = "Command tests", .tag = "command", .mod = @import("./command_tests.zig") },
 });
 
-pub fn main() void {
+pub fn main() !void {
     const verbose = if (std.os.argv.len > 1 and std.mem.eql(u8, "verbose", std.mem.span(std.os.argv[1]))) true else false;
 
-    _ = testz.runTests(Tests, verbose);
+    _ = try testz.runTests(Tests, .{ .verbose = verbose });
 }
